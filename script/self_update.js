4ae94a7a95e5c85a63849b6d62cfe0f8e9ec15c5
nhatmicls <cptprice123@outlook.com>
2023-05-07 11:47:01 +0700

    Auto update value

--- script/self_update.js
+++ script/self_update.js
@@ -13,6 +13,15 @@ function get_connection_query_url() {
     return list_url;
 }
 
+function get_toggle_query_url() {
+    const url = database_endpoint;
+    let list_url = [];
+    list_url.push(url.concat("?query=led_1_",));
+    list_url.push(url.concat("?query=led_2_",));
+
+    return list_url;
+}
+
 function verify_disconnect_device(response) {
     if (response["data"]["result"].length > 0 && disconnect_noti === 0) {
         let name_device_lost_connect = [];
@@ -55,6 +64,35 @@ function check_alert() {
     request_alert(alert_endpoint, alert_update);
 }
 
+function update_toggle_button(response) {
+    const result = response["data"]["result"]
+    if (result.length > 0) {
+        console.log(result);
+        result.forEach(element => {
+            const device_SN = element["metric"]["device_SN"];
+            const name = element["metric"]["__name__"];
+            const dash_deviceSN_location = device_SN.indexOf("_");
+            const dash_name_location = name.indexOf("_");
+            let toggle_name = "toggleLed_" + device_SN.charAt(dash_deviceSN_location + 1) + "_" + name.charAt(dash_name_location + 1);
+            if (element["value"][1] == "1") {
+                document.getElementById(toggle_name).checked = true;
+            }
+            else {
+                document.getElementById(toggle_name).checked = false;
+            }
+        });
+    }
+}
+
+function auto_update_data() {
+    const query_list = get_toggle_query_url()
+
+    //Check toggle button
+    query_list.forEach(element => {
+        request_database(element, {}, update_toggle_button);
+    })
+}
+
 setInterval(function () {
     verify_connection();
     if (disconnect_noti_ack === 1) {
@@ -66,3 +104,8 @@ setInterval(function () {
 setInterval(function () {
     check_alert();
 }, 1000); // Interval 1s
+
+setInterval(function () {
+    auto_update_data();
+}, 1000); // Interval 1s
+
